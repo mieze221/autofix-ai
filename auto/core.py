f059fc9e3c4664a02475c9d1c21a1feef85b7d35
Michael Ezeigbo Jr. <mieze4@morgan.edu>
2025-09-25 13:15:11 -0400

    Update core.py

--- auto/core.py
+++ auto/core.py
@@ -1,1 +1,12 @@
+# core.py
+def check_internet():
+    try:
+        import urllib.request
+        urllib.request.urlopen('http://google.com')
+        return True
+    except:
+        return False
+
+def hello():
+    return "AutoFix is ready!"
 
