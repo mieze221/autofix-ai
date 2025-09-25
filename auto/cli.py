29efb742a806115bdb551f6d82e1a2f98d83812f
Michael Ezeigbo Jr. <mieze4@morgan.edu>
2025-09-25 13:15:46 -0400

    Update cli.py

--- auto/cli.py
+++ auto/cli.py
@@ -1,1 +1,13 @@
+# cli.py
+from core import check_internet, hello
+
+def main():
+    print(hello())
+    if check_internet():
+        print("Internet is working!")
+    else:
+        print("No internet detected.")
+
+if __name__ == "__main__":
+    main()
 
